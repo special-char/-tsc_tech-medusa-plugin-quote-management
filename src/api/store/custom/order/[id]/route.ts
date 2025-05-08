3332f6885b375d3f6012b77d3f51badd316fa72c
Jil Sejpal <jilsejpal7@gmail.com>
2025-05-08 11:32:19 +0530

    calculate shipping charge

--- src/api/store/custom/order/[id]/route.ts
+++ src/api/store/custom/order/[id]/route.ts
@@ -1,5 +1,6 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
 import { updateDraftOrderWorkflow } from "../../../../../workflows/update-order";
+import { updateCartWorkflow } from "@medusajs/medusa/core-flows";
 
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
   try {
@@ -11,6 +12,7 @@ export async function POST(req: MedusaRequest, res: MedusaResponse) {
         })
         .status(400);
     }
+
     const query = req.scope.resolve("query");
     const orderQuery = await query.graph({
       entity: "order",
@@ -21,6 +23,8 @@ export async function POST(req: MedusaRequest, res: MedusaResponse) {
         "shipping_address.*",
         "billing_address.*",
         "metadata",
+        "cart.*",
+        "*",
       ],
       filters: { id },
     });
@@ -29,7 +33,29 @@ export async function POST(req: MedusaRequest, res: MedusaResponse) {
       id,
       orderQuery,
       ...body,
-    };
+    } as any;
+
+    const quoteData = await query.graph({
+      entity: "quotes",
+      fields: ["*"],
+      filters: { id: inputBody?.quoteId },
+    });
+
+    await updateCartWorkflow(req.scope).run({
+      input: {
+        id: quoteData.data[0].cart_id,
+
+        shipping_address: {
+          first_name: inputBody?.shipping_address?.first_name,
+          last_name: inputBody?.shipping_address?.last_name,
+          address_1: inputBody?.shipping_address?.address_1,
+          city: inputBody?.shipping_address?.city,
+          country_code: inputBody?.shipping_address?.country_code,
+          postal_code: inputBody?.shipping_address?.postal_code,
+          phone: inputBody?.shipping_address?.phone,
+        },
+      },
+    });
 
     const { result } = await updateDraftOrderWorkflow.run({
       input: inputBody,
