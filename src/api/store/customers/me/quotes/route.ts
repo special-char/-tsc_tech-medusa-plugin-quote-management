655cb8b5e07dd50a88d1f4e49c86eeae9ef12a33
Jilsejpal <jilsejpal7@gmail.com>
2025-06-04 10:19:32 +0530

    return data in asc order and version upgrade from 0.0.13 to 0.0.14

--- src/api/store/customers/me/quotes/route.ts
+++ src/api/store/customers/me/quotes/route.ts
@@ -12,13 +12,11 @@ export const GET = async (
   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
   console.log("🚀 ~ query:");
 
-  const { data: quote } = await query.graph(
-    {
-      entity: "quotes",
-      filters: { customer_id: req.auth_context.actor_id },
-      fields: req.queryConfig.fields,
-    },
-  );
+  const { data: quote } = await query.graph({
+    entity: "quotes",
+    filters: { customer_id: req.auth_context.actor_id },
+    fields: req.queryConfig.fields,
+  });
 
   const orderModuleService = req.scope.resolve(Modules.ORDER);
   const quoteData: any = [];
@@ -30,6 +28,11 @@ export const GET = async (
       const payment_status = getLastPaymentStatus(e.draft_order);
       quoteData.push({ ...e, order_preview: preview, payment_status });
     })
+  );
+
+  quoteData.sort(
+    (a, b) =>
+      new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
   );
 
   res.status(200).json(quoteData);
